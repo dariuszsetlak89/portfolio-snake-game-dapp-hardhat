709189a192d6ec690305873945252bec17b7e9ac
Dariusz Setlak <kryptoprogramista89@gmail.com>
2022-09-29 21:38:32 +0200

    SnakeToken and FruitToken contracts done

---
+++ contracts/tokens/SnakeToken.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SnakeToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("Snake Token", "SNAKE") {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner {
+        _mint(_to, _amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 0;
+    }
+}
